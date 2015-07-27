c0f245c3e8ae8a19735b57fdcd872546f19df0a2
Lucasochoa <lucasochoa96@gmail.com>
2015-07-27 15:42:23 -0400

    Development of Library
    User interface for custom buttons on its way

--- Algorythm/PlaylistMasterViewController.swift
+++ Algorythm/PlaylistMasterViewController.swift
@@ -10,17 +10,31 @@ import UIKit
 
 class PlaylistMasterViewController: UIViewController {
 
+    @IBOutlet weak var playlistImageView0: UIImageView!
     @IBOutlet weak var aButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let playlist = Playlist(index: 0)
+        playlistImageView0.image = playlist.icon
+        
         aButton.setTitle("Press Me!", forState: .Normal)
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showPlaylistDetail" {
+            
+            let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
+            playlistDetailController.segueLabelText = "Yay!"
+        }
     }
 
 }
