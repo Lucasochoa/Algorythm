e2d6c09353208abec952d9d1c409080e0209f61b
Lucasochoa <lucasochoa96@gmail.com>
2015-07-28 01:41:28 -0400

    Rough Finish of Tutorial Swift App
    rounded corners — Good UI

--- Algorythm/PlaylistMasterViewController.swift
+++ Algorythm/PlaylistMasterViewController.swift
@@ -3,24 +3,40 @@
 //  Algorythm
 //
 //  Created by Lucas Ochoa on 7/27/15.
-//  Copyright (c) 2015 Lucas Ochoa. All rights reserved.
+//  Copyright (c) 2015 Lucas Ochoa. All rights reserved./Users/Lucasochoa/Google Drive/Algorythm/Algorythm/PlaylistMasterViewController.swift
 //
 
 import UIKit
 
 class PlaylistMasterViewController: UIViewController {
+    
+    var playlistsArray: [UIImageView] = []
 
     @IBOutlet weak var playlistImageView0: UIImageView!
-    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var playlistImageView1: UIImageView!
+    @IBOutlet weak var playlistImageView2: UIImageView!
+    @IBOutlet weak var playlistImageView3: UIImageView!
+    @IBOutlet weak var playlistImageView4: UIImageView!
+    @IBOutlet weak var playlistImageView5: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        playlistsArray += [playlistImageView0,playlistImageView1,playlistImageView2,playlistImageView3,playlistImageView4,playlistImageView5]
         
-        let playlist = Playlist(index: 0)
-        playlistImageView0.image = playlist.icon
+        for thisOne in playlistsArray{
+            thisOne.layer.cornerRadius = 85.0
+        }// yay i can sortoff code^
         
-        aButton.setTitle("Press Me!", forState: .Normal)
+        for index in 0..<playlistsArray.count {
+            let playlist = Playlist(index: index)
+            let playlistImageView = playlistsArray[index]
+            playlistImageView.image = playlist.icon
+            playlistImageView.backgroundColor = playlist.backgroundColor
+        }
+        
+        playlistsArray.append(playlistImageView0)
+        
     }
 
     
@@ -30,12 +46,23 @@ class PlaylistMasterViewController: UIViewController {
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showPlaylistDetail" {
+        if segue.identifier == "showPlaylistDetailSegue" {
             
-            let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
-            playlistDetailController.segueLabelText = "Yay!"
+            
+            let playlistImageView = sender!.view as! UIImageView
+            
+            if let index = find(playlistsArray, playlistImageView){
+                let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
+                playlistDetailController.playlist = Playlist(index: index)
+            }
+            
         }
     }
 
+    @IBAction func showPlaylistDetail(sender: AnyObject) {
+    println("working")
+        performSegueWithIdentifier("showPlaylistDetailSegue", sender: sender)
+
+    }
 }
 
